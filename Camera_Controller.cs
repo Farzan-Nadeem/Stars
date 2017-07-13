289384d1e4713a89a9babbe798f058f34cbaf02f
Farzan Nadeem <mfnadeem@edu.uwaterloo.ca>
2017-07-13 13:51:20 -0400

    PlayerController CameraController

---
+++ Camera_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+   	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {                        ///Runs after all items have been processed in update, so will 
+                                                ///definetly run after the player object has ran
+        transform.position = player.transform.position + offset;
+	}
+} 
---
+++ Player_Controller.cs
@@ -0,0 +1,36 @@
+﻿ using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    private Rigidbody rb;       ///Field for the component Rigid Body
+
+    public float speed;
+    public float flyingHeight;
+    public float turningSpeed;
+    public float turnCatchSpeed;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();             ///Fetches the component from Unity as soon as the obj comes onto the frame
+    }
+
+    void FixedUpdate(){
+
+        float moveHorizontal = Input.GetAxis("Horizontal");        
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 acceleration = new Vector3(0, 0, moveVertical);
+        Vector3 rotate = new Vector3(0, moveHorizontal, 0);
+        Vector3 rotateCatch = new Vector3(moveHorizontal, 0, 0);
+
+        rb.AddRelativeForce(acceleration * speed, ForceMode.Acceleration);
+        rb.AddForce(new Vector3(0, flyingHeight, 0));
+        rb.AddRelativeForce(rotateCatch * turnCatchSpeed);
+
+        transform.Rotate(rotate * Time.deltaTime * turningSpeed);
+
+
+       }
+
+}
